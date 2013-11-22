dc525e83bc9bd33ec31286bf4c533ab683d991fa
Maksim Fomin <hellow68@gmail.com>
2013-11-22 12:57:57 +0400

    Создан класс реализующий 2д физ. модель

--- endoftheline/src/com/strangeiron/endoftheline/components/Eotl2DModel.java
+++ endoftheline/src/com/strangeiron/endoftheline/components/Eotl2DModel.java
@@ -2,32 +2,104 @@
 package com.strangeiron.endoftheline.components;
 
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.strangeiron.endoftheline.physics.CircleModel;
 import com.strangeiron.endoftheline.physics.PolygonModel;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Eotl2DModel {
+public class Eotl2DModel implements Cloneable {
     public String name;
     public String texture;
-    public final Vector2 origin = new Vector2();
+    public float scale = 1;
+    public Vector2 origin = new Vector2();
     public final List<PolygonModel> polygons = new ArrayList<PolygonModel>();
     public final List<CircleModel> circles = new ArrayList<CircleModel>();
+    private final List<Vector2> vectorPool = new ArrayList<Vector2>();
+    private final PolygonShape polygonShape = new PolygonShape();
+    private final CircleShape circleShape = new CircleShape();
+    private final List<FixtureDef> fixtures = new ArrayList<FixtureDef>();
     
     public Eotl2DModel()
     {
         
     }
 
-    // pre-build model
-    public void init()
+    public void applyToBody(Body body)
     {
-        
+        for (int i = 0; i < body.getFixtureList().size; i++) {
+            body.destroyFixture(body.getFixtureList().get(i));
+        }
+        for (int i = 0; i < fixtures.size(); i++) {
+            body.createFixture(fixtures.get(i));
+        }
     }
     
-    // get scaled model
-    public void getScaledModel(Float scale)
+    public void generateCollider(float Scale)
     {
+        Vector2 origincaled = origin.scl(Scale);
         
+        for (int i = 0; i < polygons.size(); i++) {
+            PolygonModel polygon = polygons.get(i);
+            Vector2[] vertices = polygon.buffer;
+            
+            for (int j = 0; j < vertices.length; j++) {
+                vertices[j] = newVec().set(polygon.vertices.get(j)).scl(Scale);
+                vertices[j].sub(origincaled);
+            }
+            
+            polygonShape.set(vertices);
+            
+            FixtureDef fd = new FixtureDef();
+            fd.shape = polygonShape;
+            fixtures.add(fd);
+            
+            for (int j = 0; j < vertices.length; j++) {
+                free(vertices[j]);
+            }
+        }
+        
+        for (int i = 0; i < circles.size(); i++) {
+            CircleModel circle = circles.get(i);
+            
+            Vector2 center = circle.center.scl(Scale);
+            float radius = circle.radius * Scale;
+            
+            circleShape.setPosition(center);
+            circleShape.setRadius(radius);
+            
+            FixtureDef fd = new FixtureDef();
+            fd.shape = circleShape;
+            fixtures.add(fd);
+            
+            free(center);
+        }
+        
+        scale = Scale;
+    }
+    
+    public void scale()
+    {
+        generateCollider(scale);
+    }
+    
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+    
+    /**
+     * -----> Utils
+     */
+    private Vector2 newVec() {
+            return vectorPool.isEmpty() ? new Vector2() : vectorPool.remove(0);
+    }
+
+    private void free(Vector2 v) {
+            vectorPool.add(v);
     }
 }
